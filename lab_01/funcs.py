c43b41269fcd22face5d1a32b4b4b876d3c12ae6
Sultan <romtrash98@gmail.com>
2020-09-20 20:56:28 +0300

    quick sort was added

--- lab_01/funcs.py
+++ lab_01/funcs.py
@@ -18,15 +18,47 @@ def naive_poly_function(v: list, x: float = 1.5) -> float:
 
 
 def gorner_method_function(v: list, x: float = 1.5) -> float:
-    def gorner_sub(lst: list, value: float, aggregate: float) -> float:
-        if len(lst) == 0:
-            return aggregate
-        else:
-            agg = lst[-1] + value * aggregate
-            return gorner_sub(lst[:-1], value, agg)
+    return reduce(lambda agg, a: a + x * agg, reversed(v), 0)
 
-    return gorner_sub(v, x, 1)
+
+def bubble_sort(v: list) -> list:
+    for i in range(len(v) - 1):
+        for j in range(len(v) - i - 1):
+            if v[j] > v[j + 1]:
+                v[j], v[j + 1] = v[j + 1], v[j]
+
+    return v
+
+
+def quick_sort(v: list) -> list:
+    def partition(lst: list, lw: int, hg: int) -> int:
+        i, j = lw, hg + 1
+        while True:
+            while True:
+                i += 1
+                if not(lst[i] < lst[lw]) or i == hg:
+                    break
+            while True:
+                j -= 1
+                if not(lst[j] > lst[lw]) or j == lw:
+                    break
+            if i >= j:
+                break
+            lst[i], lst[j] = lst[j], lst[i]
+        lst[lw], lst[j] = lst[j], lst[lw]
+        return j
+
+    def quick_sort_routine(lst: list, lw: int, hg: int) -> None:
+        if hg <= lw:
+            return
+        j = partition(lst, lw, hg)
+        quick_sort_routine(lst, lw, j - 1)
+        quick_sort_routine(lst, j + 1, hg)
+
+    quick_sort_routine(v, 0, len(v) - 1)
+    return v
 
 
 if __name__ == '__main__':
-    print(gorner_method_function([1, 2, 3]))
+    print(bubble_sort([3, 2, 1]))
+    print(quick_sort([3, 2, 1]))
