881717ca9596058ea6a681accfa85d03384eef13
Sultan Zhumabaev <romtrash98@gmail.com>
2020-09-21 17:05:00 +0300

    plot was added

--- lab_01/main.py
+++ lab_01/main.py
@@ -1,11 +1,30 @@
+import argparse
 import os
+from datetime import datetime
 from random import random, seed
 from timeit import timeit
 from typing import List
 
 import pandas as pd
+from scipy.signal import cspline1d
+from matplotlib import pyplot as plt
+from numpy import polyfit, ndarray, asarray
 
 import lab_01
+
+
+seed()
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser(prog="lab_01")
+    parser.add_argument("-c", "--csv_path", type=str, help="Path to csv of dataframe")
+    parser.add_argument("-s", "--skip_methods", nargs="*", type=str, help="List of ignored methods",
+                        choices=lab_01.__all__, default=["prod_matrix", "vinograd_mod"])
+    parser.add_argument("-n", type=int, help="Max vector size", default=2000)
+    parser.add_argument("-sd", "--step_dump", type=int, help="Step of dump", default=100)
+    parser.add_argument("-sr", "--step_run", type=int, help="Step of run", default=1)
+    return parser.parse_args()
 
 
 def get_random_matrix(n: int, m: int) -> List[list]:
@@ -21,42 +40,54 @@ def matrix_copy(matr: List[list]) -> List[list]:
 
 
 if __name__ == '__main__':
-    seed()
-    df = pd.DataFrame(columns=lab_01.__all__, dtype='float')
-    n = 100
-    step = 10
+    args = get_argparser()
 
-    skip_function = set()
+    n = args.n
+    step_run = args.step_run
+    if args.csv_path:
+        df = pd.read_csv(args.csv_path, sep=';', header=0, index_col=0)
+    else:
+        function_set = set(lab_01.__all__) - set(args.skip_methods)
+        skip_func = list()
+        df = pd.DataFrame(columns=function_set, dtype='float')
 
-    if not os.path.isdir("dumps"):
-        os.mkdir("dumps")
+        step_dump = args.step_dump
 
-    for i in range(1, n + 1):
-        vector = get_random_vector(i)
-        matrix_a = get_random_matrix(i, i)
-        matrix_b = get_random_matrix(i, i)
+        if not os.path.isdir("dumps"):
+            os.mkdir("dumps")
 
-        row = dict()
-        for foo in filter(lambda x: x not in skip_function, lab_01.__all__):
-            time_s = None
-            try:
-                time_s = timeit(f"{foo}(vector)", setup=f"from lab_01 import {foo}", number=5,
-                                globals=dict(vector=vector.copy()))
-            except (TypeError, ValueError):
-                time_s = timeit(f"{foo}(matrix_a, matrix_b)", setup=f"from lab_01 import {foo}", number=5,
-                                globals=dict(matrix_a=[r.copy() for r in matrix_a],
-                                             matrix_b=[r.copy() for r in matrix_b]))
-            except OverflowError:
-                skip_function.add(foo)
-            row[foo] = time_s
+        for i in range(1, n + 1, step_run):
+            vector = get_random_vector(i)
+            matrix_a = get_random_matrix(i, i)
+            matrix_b = get_random_matrix(i, i)
 
-        for foo in skip_function:
-            row[foo] = None
+            row = dict()
+            for foo in filter(lambda f: f not in skip_func, function_set):
+                time_s = None
+                try:
+                    time_s = timeit(f"{foo}(vector)", setup=f"from lab_01 import {foo}", number=5,
+                                    globals=dict(vector=vector.copy()))
+                except (TypeError, ValueError):
+                    time_s = timeit(f"{foo}(matrix_a, matrix_b)", setup=f"from lab_01 import {foo}", number=5,
+                                    globals=dict(matrix_a=[r.copy() for r in matrix_a],
+                                                 matrix_b=[r.copy() for r in matrix_b]))
+                except OverflowError:
+                    skip_func.append(foo)
+                row[foo] = time_s
 
-        df.loc[i] = row
+            for foo in set(lab_01.__all__) - function_set:
+                row[foo] = None
 
-        if i % step == 0:
-            print(f"save dump: n = {i}")
-            df.to_csv(f"dumps/{i}_dump.csv", sep=";")
+            df.loc[i] = row
 
-    print(df[df.columns[-3:]])
+            if i % step_dump == 0 or i + step_run > n:
+                print(f"save dump: n = {i}")
+                df.to_csv(f"dumps/{datetime.now().strftime('%Y%m%d')}_{i}.csv", sep=";")
+
+    fig, ax = plt.subplots(3, 3)
+    df.fillna(method='pad')
+    df.plot(subplots=True, ax=ax, grid=True, title="Time complexity", color="m")
+    # ax[2, 2].plot(polyfit(df[df.columns[8]], df.index, 19), df[df.columns[8]], color="c")
+    ax[1, 0].plot(df.index, cspline1d(asarray(df[df.columns[3]].to_list()), 100), color="c")
+    plt.subplots_adjust(0.075, 0.1, 0.95, 0.95)
+    plt.show()
