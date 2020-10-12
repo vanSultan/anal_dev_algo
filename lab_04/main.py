e16286db6d4ccc03e0f0e95329e4ca28dc2e18ed
Sultan <romtrash98@gmail.com>
2020-10-12 22:54:18 +0300

    fix call

--- lab_04/main.py
+++ lab_04/main.py
@@ -33,15 +33,15 @@ if __name__ == '__main__':
     print_log("Nelder-Mead", co_1.x, co_1.nit, co_1.nfev)
     plt.plot(x, [rational_approx(x_i, *co_1.x) for x_i in x], color="m", label="Nelder-Mead")
 
-    co_2 = least_squares(f_least_squares, array([-0.1, 0.1, -0.1, 0.1]), args=(rational_approx, signal))
+    co_2 = least_squares(f_least_squares, array([-1, 1, 1, 1]), args=(rational_approx, signal), xtol=eps, max_nfev=1000)
     print_log("Levenberg-Marquardt", co_2.x, NaN, co_2.nfev)
     plt.plot(x, [rational_approx(x_i, *co_2.x) for x_i in x], color="r", label="Levenberg-Marquardt")
 
-    co_3 = dual_annealing(f_least_squares, [(-5, 5), (-5, 5), (-5, 5), (-5, 5)], (rational_approx, signal))
+    co_3 = dual_annealing(f_least_squares, [(-5, 5), (-5, 5), (-5, 5), (-5, 5)], (rational_approx, signal), maxiter=1000)
     print_log("Simulated Annealing", co_3.x, co_3.nit, co_3.nfev)
     plt.plot(x, [rational_approx(x_i, *co_3.x) for x_i in x], color="y", label="Simulated Annealing")
 
-    co_4 = differential_evolution(f_least_squares, [(-5, 5), (-5, 5), (-5, 5), (-5, 5)], (rational_approx, signal))
+    co_4 = differential_evolution(f_least_squares, [(-5, 5), (-5, 5), (-5, 5), (-5, 5)], (rational_approx, signal), maxiter=1000, tol=eps)
     print_log("Differential Evolution", co_4.x, co_4.nit, co_4.nfev)
     plt.plot(x, [rational_approx(x_i, *co_4.x) for x_i in x], color="g", label="Differential Evolution")
 
